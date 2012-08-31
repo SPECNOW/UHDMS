a623c999f47e9b9df336ee24523cd781bf40735d
Lee <Lee@Lee-PC>
2012-08-30 20:17:07 -0700

    Working ADC (3 Channels)
    - corrected error where all ADC values were tied together (same). Solved
    by making variables global
    
    - Fixed Broken sensor. Can be noted if value stays high (~800) for
    future reference.
    
    - One of the ground pins (#2) on the ground node doesn't work
    
    - Tested current code and the stopping condition works, values need to
    be tweaked

--- PWMTest/main.c
+++ PWMTest/main.c
@@ -11,7 +11,7 @@
 #define F_RIGHT 4
 #define R_LEFT 5
 #define R_RIGHT 6
-//TESTING LINE
+
 typedef struct{
 	char Speed; //0-255
 	char Dir; // Use defines above
@@ -40,12 +40,21 @@ int setDir(Car* pThis,char dir)
 	return 0;
 }
 
+void setLane(Car* pThis, int lane)
+{
+	pThis->lane = lane;
+}
+
 void InitPWM();
 void init(void);
-void ADC_1(void);
-void ADC_2(void);
+unsigned int ADC_1();
+unsigned int ADC_2();
+unsigned int ADC_3();
+unsigned int ADC1_ = 0;
+unsigned int ADC2_ = 0;
+unsigned int ADC3_ = 0;
+unsigned int forward = 0, left = 0, right = 0;
 
-unsigned int PWM1_value, PWM2_value;
 Car car;
 void main(void)
 {
@@ -53,12 +62,74 @@ void main(void)
 	InitPWM();
 	setDir(&car,STOP); //STOP Car
 	setSpeed(&car,127); //50%
+	setLane(&car, 1);
+
+	//DEFINE VARIABLES
+	int i = 0;
 
 	__bis_SR_register(GIE);
 	while(1)
 	{
-		ADC_1();
-		ADC_2();
+		ADC1_ = ADC_1();//Forward
+		forward = 500;
+		if(ADC1_ > forward/2)//tweak
+		{
+			setDir(&car,STOP);
+			i += 1;
+			if(i == 1000000)
+			{
+				//setDir(&car,REVERSE);
+				//__delay_cycles(50000);
+			}
+		}
+		else
+			setDir(&car, FORWARD);
+		ADC2_= ADC_2();//Left
+		if(ADC2_ < forward/2)//tweak
+		{
+			setDir(&car,STOP);
+			i += 1;
+			if(i == 1000000)
+			{
+				//setDir(&car,REVERSE);
+				//__delay_cycles(50000);
+			}
+		}
+		ADC3_= ADC_3();//Right
+		if(ADC3_ < forward/2)//tweak
+		{
+			setDir(&car,STOP);
+			i += 1;
+			if(i == 1000000)
+			{
+				//setDir(&car,REVERSE);
+				//__delay_cycles(50000);
+			}
+		}
+	/*	if(ADC1_ > forward && ADC2_ > left && ADC3_ > right)
+		{
+
+		}
+		else if(ADC1_ < forward)
+		{
+			//slow down
+			if(ADC2_ > ADC3_)
+			{
+				//Turn Right
+			}
+			else//NOTE: When ADC2_ = ADC3_, it turns left
+			{
+				//Turn Left
+			}
+		}
+		else if(ADC1_ < forward && ADC2_ < left && ADC3_ < right)
+		{
+			//Stop
+			//delay
+			//check ADC again, turn left or right or reverse
+		}*/
+		//delay
+		//__delay_cycles(10000);
 	}
 }
 
@@ -86,45 +157,63 @@ void init(void)
 	WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
 
 	ADC10CTL0 = ADC10SHT_2 + MSC + ADC10ON + ADC10IE; // Sample Hold Time2, Multiple Seq Channel, ADC10ON, Interrupt Enabled
-  	ADC10DTC1 = 0x02;
+  	ADC10DTC1 = 0x03;						//3 Conversions
   	ADC10AE0 |= 0x07;                         // P2.0, P2.1, and P2.2 ADC option select (3 ADCs turned on)
   	P1DIR |= 0x03;
 }
 
-void ADC_1(void)
+unsigned int ADC_1()
 {
+	int ADC1_value;
 	ADC10CTL0 &= ~ENC;                      // Disable conversion
 	ADC10CTL1 = INCH_0 + CONSEQ_0;          // Select Channel 0 , Single Channel single conversion
 	ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
 	while( ADC10CTL1 & ADC10BUSY );         // wait until conversion is done
 	ADC10CTL0 &= ~ENC;
-    PWM1_value = 760 - ADC10MEM;
-	if ( PWM1_value <= 320)
-	{
-		setDir(&car,FORWARD);
-		P1OUT ^=0x01;
-	}
-	else
-	{
-		setDir(&car,REVERSE);
-	}
+    ADC1_value = ADC10MEM;
+//	if ( ADC1_value >= 320)
+//	{
+//		setDir(&car,FORWARD);
+//		P1OUT ^=0x02;
+//	}
+	ADC10CTL1 &= ~INCH_0;
+	return ADC1_value;
 }
 
-void ADC_2(void)
+unsigned int ADC_2()
 {
  	//2ND ADC
+	unsigned int ADC2_value;
   	ADC10CTL0 &= ~ENC;                      // Disable conversion
 	ADC10CTL1 = INCH_1 + CONSEQ_0;          // Select Channel 1 , Single Channel single conversion
 	ADC10CTL0 |= ENC + ADC10SC;             // Sampling and conversion start
 	while( ADC10CTL1 & ADC10BUSY );         // wait until conversion is done
 	ADC10CTL0 &= ~ENC;
-    PWM2_value = 760 - ADC10MEM;
-  	if (PWM2_value <= 500)
+    ADC2_value = ADC10MEM;
+  	if (ADC2_value <= 500)
   	{
-  		P1OUT ^=0x02;
+  		//P1OUT ^=0x01;
   	}
+  	ADC10CTL1 &= ~INCH_1;
+  	return ADC2_value;
 }
 
+unsigned int ADC_3()
+{
+	//3rd ADC
+	unsigned int  ADC3_value;
+	ADC10CTL0 &=~ENC;
+	ADC10CTL1 = INCH_2 + CONSEQ_0;
+	ADC10CTL0 |= ENC + ADC10SC;
+	while( ADC10CTL1 & ADC10BUSY );
+    ADC3_value = ADC10MEM;
+  	if (ADC3_value <= 500)
+  	{
+  		//P1OUT ^=0x02;
+  	}
+  	ADC10CTL1 &= ~INCH_2;
+  	return ADC3_value;
+}
 #pragma vector = TIMERB1_VECTOR
 __interrupt void TB1_ISR(void)
 {
